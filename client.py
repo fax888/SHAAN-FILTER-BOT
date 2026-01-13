245e90cd6892a64e24d3f6589f1ec342b4743e75
Arush <alan2.008y@gmail.com>
2026-01-13 06:51:59 +0530

    client.py

---
+++ client.py
@@ -0,0 +1,9 @@
+from pyrogram import Client
+import os
+
+client = Client(
+    "bot",
+    api_id=int(os.getenv("API_ID")),
+    api_hash=os.getenv("API_HASH"),
+    bot_token=os.getenv("BOT_TOKEN")
+)
