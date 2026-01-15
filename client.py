68701f50811baa2042a0a8caf640b87b50832da6
Arush <alan2.008y@gmail.com>
2026-01-15 06:39:55 +0530

    Delete client.py

--- client.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client
-import os
-
-client = Client(
-    "bot",
-    api_id=int(os.getenv("API_ID")),
-    api_hash=os.getenv("API_HASH"),
-    bot_token=os.getenv("BOT_TOKEN")
-)
