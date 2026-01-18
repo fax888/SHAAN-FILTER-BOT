33e48f164c7b840d61aba7e3c8b53cf8d335f9b2
Arush <alan2.008y@gmail.com>
2026-01-18 15:03:05 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters, enums
 from pyrogram.types import ChatJoinRequest, Message
-from info import REQ_CHANNEL,ADMINS
+from info import REQST_CHANNEL,ADMINS
 from database.users_chats_db import db
 
 
